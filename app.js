418eb454e3c4d2e33b64d7332995b7585b6a9f1a
2021BTech <daffydanni@gmail.com>
2023-09-18 08:08:59 +0100

    challenge 5

--- app.js
+++ app.js
@@ -18,8 +18,18 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// home route
 app.get("/", function (req, res) {
   res.render("home", { startingContent: homeStartingContent });
+});
+// about route
+app.get("/about", function (req, res) {
+  res.render("about", { startingContent: aboutContent });
+});
+
+// contact route
+app.get("/contact", function (req, res) {
+  res.render("contact", { startingContent: contactContent });
 });
 
 app.listen(5000, function () {
